2df732b54fd3b6f6c60a9bd55dd0538b71f45466
Alli Campbelll <allison.campbell@ou.edu>
2017-04-25 11:00:40 -0500

    adding a this property

--- dice-roll/diceRoll.js
+++ dice-roll/diceRoll.js
@@ -1,11 +1,26 @@
 
+//simple object
 //create a method to encapsulate the function
 var dice = {
+    // sides is a property of dice
     sides: 6,
     //use an anonymous function
     roll: function () {
         var sides = 6;
-        var randomNumber = Math.floor(Math.random() * dice.sides) + 1;
+        var randomNumber = Math.floor(Math.random() * this.sides) + 1;
     console.log(randomNumber);
     }
 }
+
+
+var dice10 = {
+//change property to 10
+    sides: 10,
+    roll: function () {
+        var sides = 6;
+// this always references the owner of the object
+        var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    console.log(randomNumber);
+    }
+}
+
