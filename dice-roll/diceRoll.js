658dbad261ea10a7b0074007a9d5c8451f194f5f
Alli Campbelll <allison.campbell@ou.edu>
2017-04-25 11:47:54 -0500

    adding the ui return method

--- dice-roll/diceRoll.js
+++ dice-roll/diceRoll.js
@@ -6,21 +6,9 @@ var dice = {
     sides: 6,
     //use an anonymous function
     roll: function () {
-        var sides = 6;
         var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    console.log(randomNumber);
+        return (randomNumber);
     }
 }
 
-
-var dice10 = {
-//change property to 10
-    sides: 10,
-    roll: function () {
-        var sides = 6;
-// this always references the owner of the object
-        var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    console.log(randomNumber);
-    }
-}
 
