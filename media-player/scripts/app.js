97cae6190d166aaac7d47459b01c430584a6a8c4
Alli Campbelll <allison.campbell@ou.edu>
2017-05-02 14:13:08 -0500

    adding media player files

---
+++ media-player/scripts/app.js
@@ -0,0 +1,7 @@
+var playlist = new Playlist();
+
+var caladan = new Song("Caladan", "Grimes", "2:26");
+var nothingMoreToSay = new Song ("Nothing More to Say", "SOPHIE", "5:25");
+
+playlist.add(caladan);
+playlist.add(nothingMoreToSay);
