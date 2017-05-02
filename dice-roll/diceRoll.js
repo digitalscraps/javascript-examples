ab31ec4f4f77e016d6f9e2dfc7dea48fde5848c7
Alli Campbelll <allison.campbell@ou.edu>
2017-05-02 13:10:56 -0500

    updating protype method

--- dice-roll/diceRoll.js
+++ dice-roll/diceRoll.js
@@ -6,7 +6,7 @@ function Dice(sides) {
 Dice.prototype.roll = function () {
         var randomNumber = Math.floor(Math.random() * this.sides) + 1;
         return randomNumber;
-    }
+}
 
 var dice = new Dice(6);
 var dice = new Dice(10);
